664c068c7b5deb9f2c6b65aab099eda1b4667af1
Chase Waggener <tidalpaladin@gmail.com>
2018-08-30 11:03:34 -0500

    start implementing gui

---
+++ src/gui.py
@@ -0,0 +1,11 @@
+import stoploss
+import plotly
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+
+
+def generate_table(data):
+    return html.Table(
+        [html.Tr(html.Th(col)) for col in]
+    )
