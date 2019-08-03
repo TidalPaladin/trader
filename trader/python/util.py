24bc457abce169efe7467863dbc4dcd84ac859e5
Scott Chase Waggener <tidalpaladin@protonmail.com>
2019-08-03 16:54:03 -0500

    Restructure callback get method + logging

--- trader/python/util.py
+++ trader/python/util.py
@@ -12,6 +12,10 @@ from datetime import datetime
 from tensorflow.keras.callbacks import ModelCheckpoint, ProgbarLogger
 import tensorflow.feature_column as fc
 from tensorboard.plugins.hparams import api as hp
+from absl import logging
+
+
+DATE = datetime.now().strftime("%Y%m%d-%H%M%S")
 
 @tf.function
 def standardize(f, axis=-1):
@@ -28,7 +32,34 @@ def get_callbacks(FLAGS):
     DATE = datetime.now().strftime("%Y%m%d-%H%M%S")
 
     checkpoint_dir = os.path.join(FLAGS.artifacts_dir, 'checkpoint', DATE)
+    logging.info("Model checkpoint dir: %s", checkpoint_dir)
     tb_dir = os.path.join(FLAGS.artifacts_dir, 'tblogs')
+    logging.info("Tensorboard log dir: %s", tb_dir)
+
+
+    # Reduce learning rate if loss isn't improving
+    learnrate_args = {
+            'monitor':'loss',
+            'factor': 0.5,
+            'patience': 3,
+            'min_lr': 0.0001
+    }
+    logging.info("ReduceLROnPlateau: %s", learnrate_args)
+    learnrate_cb = tf.keras.callbacks.ReduceLROnPlateau(**learnrate_args)
+
+    # Stop early if loss isn't improving
+    stopping_args = {
+            'monitor':'loss',
+            'min_delta': 0.001,
+            'patience': 5,
+    }
+    logging.info("EarlyStopping: %s", learnrate_args)
+    stopping_cb = tf.keras.callbacks.EarlyStopping(**stopping_args)
+
+    # Skip IO callbacks if requested
+    if FLAGS.dry:
+        return [learnrate_cb, stopping_cb ]
+
     os.makedirs(tb_dir, exist_ok=True)
     os.makedirs(checkpoint_dir, exist_ok=True)
 
